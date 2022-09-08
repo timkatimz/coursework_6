7833d104ead5d42dc57cdec25714393b3ebbf4f4
kpaveliev <kpaveliev@gmail.com>
2022-09-08 13:35:34 +0300

    djoser and user models

--- backend_django/backend_django/settings.py
+++ backend_django/backend_django/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'djoser',
     'ads',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +129,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DJOSER
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'users.serializers.UserRegistrationSerializer'
+    },
+    'LOGIN_FIELD': 'email'
+}
