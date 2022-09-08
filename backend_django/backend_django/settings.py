f253a29909675d577ef7746747513bc636e2eaf9
kpaveliev <kpaveliev@gmail.com>
2022-09-08 13:57:47 +0300

    image added to users

--- backend_django/backend_django/settings.py
+++ backend_django/backend_django/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,6 +125,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/django_media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'django_media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -138,3 +141,5 @@ DJOSER = {
     },
     'LOGIN_FIELD': 'email'
 }
+
+AUTH_USER_MODEL = 'users.User'
