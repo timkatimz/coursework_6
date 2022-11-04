49d3471d251f9103331b5d9723827ec9e64e0157
tim <timrhymer@ya.ru>
2022-11-04 15:27:11 +0400

    COMPLETE

--- backend_django/backend_django/settings.py
+++ backend_django/backend_django/settings.py
@@ -176,3 +176,10 @@ DJOSER = {
 }
 
 AUTH_USER_MODEL = 'users.User'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
